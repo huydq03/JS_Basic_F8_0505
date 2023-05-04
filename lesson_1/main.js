3e7431f4ab03fbb8f8dcf3f514a3e93c96f13580
huydq03 <huydo0215@gmail.com>
2023-05-05 00:40:50 +0700

    lesson1

---
+++ lesson_1/main.js
@@ -0,0 +1,15 @@
+/**
+ * Giới thiệu về built-in
+ * 1. Alert
+ * 2. Console
+ * 3. confirm
+ * 4. prompt
+ * 5. set timeout
+ * 6. set interval
+ */
+// khai báo biến và lưu giá trị
+var fullName = "Do Quoc Huy";
+var age = 20; //khai báo tuổi
+// gọi hàm thông báo
+alert(fullName);
+alert(age);
